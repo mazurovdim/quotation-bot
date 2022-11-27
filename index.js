2d119e73be4a8e9a35d17d19634f2a12295d8167
Dmitriy <mazurovdimon@gmail.com>
2022-11-27 20:24:00 +0300

    Написан вызов кнопок выбора при вызове команды

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Telegram } from "telegraf";
 import dotenv from 'dotenv'
 import help from "./modules/help.js";
 import start from "./modules/start.js";
@@ -6,8 +6,23 @@ import start from "./modules/start.js";
 const env = dotenv.config().parsed
 const botToken = env.TELEGRAM_BOT_TOKEN
 const bot = new Telegraf(botToken);
+const telegram = new Telegram(botToken, {
+    agent: null,        // https.Agent instance, allows custom proxy, certificate, keep alive, etc.
+    webhookReply: true  // Reply via webhook
+  })
 
-bot.start(start);
+bot.start();
+
+bot.command('gg', context => {
+    bot.telegram.sendMessage(context.chat.id, 'Найти филосовскую фразу?', 
+    {
+        reply_markup:{
+           inline_keyboard:[[{text:'Da', callback_data:'zzz'},{text:'Net', callback_data:'bbb'}]]
+        }
+            
+    })
+})
+
 bot.help(help)
 
 
