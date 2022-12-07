c2464beea3a767529bc8bfe11e323fbbd7d910e6
Dmitriy Mazurov <mazurovdimon@gmail.com>
2022-12-07 10:57:53 +0300

    Добавлен тестовый вариант работы с лайками

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Telegraf, Markup } from "telegraf";
 import dotenv from 'dotenv'
 import help from "./modules/help.js";
 import start from "./modules/start.js";
@@ -15,5 +15,16 @@ bot.help(help)
 bot.hears('Выбрать фразу', getFromAll)
 bot.hears(/[А-Яа-яЁё]+/, getFiltered)
 bot.hears(/[A-Za-z0-9\W]+/, (ctx)=> ctx.reply(Emoji.emojify(':point_up_2: Братуха, на пендосском не понимаем!')))
+bot.action('like',async (ctx) => {
+    await ctx.answerCbQuery()
+    let count = parseInt(ctx.callbackQuery.message.reply_markup.inline_keyboard[0][0].text) 
+    await ctx.editMessageReplyMarkup(
+            {
+                inline_keyboard: [[Markup.button.callback(`${++count}`,'like')]]
+            }
+        )
+    console.log(ctx.callbackQuery.message.reply_markup.inline_keyboard[0][0].text)
+})
+bot.action('liked',async ctx => await ctx.answerCbQuery())
 
 bot.launch();
--- modules/random-phrase.js
+++ modules/random-phrase.js
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs'
 import  Emoji from "node-emoji";
+import { Markup } from 'telegraf';
 const phaseArray = readFileSync('assets/quotations.txt','utf-8').split('\n')
 
 function getRandomPhrase(arr = phaseArray){
@@ -8,13 +9,15 @@ function getRandomPhrase(arr = phaseArray){
 }
 
 export function getFromAll (context) {
-    context.reply(getRandomPhrase()) 
+    context.reply(getRandomPhrase(), Markup.inlineKeyboard([
+        Markup.button.callback('0','like')
+    ]))
 }
 
 export function getFiltered (context){
     const word = context.message.text
     const filtered = phaseArray.filter(phrase => phrase.toLowerCase().includes(word.toLowerCase()))
-    if(!filtered.length) return context.reply(Emoji.emojify(':exclamation: Братуха, такую фразу еще не подвезли. Заходи завтра')) 
+    if(!filtered.length) return context.reply(Emoji.emojify(':exclamation: Братуха, такую фразу еще не подвезли. Заходи завтра'))
     context.reply(getRandomPhrase(filtered)) 
 }
 
